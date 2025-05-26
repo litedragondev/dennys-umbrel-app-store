557b67fc5b40642ca3a7e7009d000fc42a0a5800
Denny Subke <155585746+dennysubke@users.noreply.github.com>
2025-05-26 12:42:09 +0200

    Update exports.sh

--- denny-spis/exports.sh
+++ denny-spis/exports.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-UMBREL_ROOT="${UMBREL_ROOT:-/mnt/data/umbrel}"
-APP_DATA_DIR="${UMBREL_ROOT}/data"
+UMBREL_DATA_DIR="${UMBREL_ROOT}/data"
+UMBREL_DATA_STORAGE_DIR="${UMBREL_DATA_DIR}/storage"
 SPIS_DATA_DIR="${APP_DATA_DIR}/data/spis_data"
-SPIS_MEDIA_DIR="${UMBREL_ROOT}/data/storage"
-DESIRED_OWNER="1000:1000"
 
-mkdir -p "${SPIS_DATA_DIR}" "${SPIS_MEDIA_DIR}"
-
-correct_owner() {
-  local dir="$1"
-  if [[ -d "$dir" ]]; then
-    local owner
-    owner=$(stat -c "%u:%g" "$dir")
-    if [[ "$owner" != "$DESIRED_OWNER" ]]; then
-      chown -R "$DESIRED_OWNER" "$dir"
-    fi
-  fi
-}
-
-correct_owner "${SPIS_DATA_DIR}"
-correct_owner "${SPIS_MEDIA_DIR}"
+mkdir -p "${UMBREL_DATA_STORAGE_DIR}"
+mkdir -p "${SPIS_DATA_DIR}"
