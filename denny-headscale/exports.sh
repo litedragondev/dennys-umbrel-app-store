6bc183bdcbf3f4159ac822de172e5faccb541bf5
Denny Subke <155585746+dennysubke@users.noreply.github.com>
2025-06-08 18:37:38 +0200

    Delete denny-headscale directory

--- denny-headscale/exports.sh
+++
@@ -1,6 +0,0 @@
-# Replace with your Umbrel's IP or host name
-export UMBREL_HOST="172.17.0.3" # Example 172.17.0.2
-
-# Headplate Configs
-HEADPLANE_LOAD_ENV_OVERRIDES=true
-HEADPLANE_HEADSCALE__URL="http://172.17.0.3:37070"
